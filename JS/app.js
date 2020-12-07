af110f70e417ae75b185d9e7afd8d28601646000
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2020-12-07 18:14:17 +0100

    Slutet av projekt

--- JS/app.js
+++ JS/app.js
@@ -1,100 +1,5 @@
-/************* MUHANNAD'S DEL PÅBÖRJAR **************/
-
-document.querySelector('.control-button button').onclick = function (){
-
-  const name = prompt('Whats your name?');
-
-  if(name == null || name == ''){
-
-    document.querySelector('.name .playerName').innerHTML = 'Unknown';
-
-  }else{
-
-    document.querySelector('.name .playerName').innerHTML = name;
-    
-  }
-
-  document.querySelector('.control-button').remove()
-
-}
-  
-/************* TIMER FUNCTIONS START **************/
-
-let clockId;
-let clockOff = true;
-let time = 0;
-
-function displayTime() {
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
-  const clock = document.querySelector('.clock');
-  clock.innerHTML = time;
-  if (seconds < 10) {
-      clock.innerHTML = `${minutes}:0${seconds}`;
-  } else {
-      clock.innerHTML = `${minutes}:${seconds}`;
-  }
-}
-
-function startClock() { 
-  clockId = setInterval( function() {
-      time++;
-      displayTime();
-  }, 1000);
-}
-
-function stopClock(){
-  clearInterval(clockId);
-  clockOff = true;
-};
-
-function resetTimer(){
-  stopClock()
-  clockOff = true;
-  time = 0;
-  displayTime();
-}
-
-/************* TIMER FUNCTIONS END **************/
-
-/************* RESET FUNCTION START **************/
-
-function reset(){
-  resetTimer();
-  resetMoves();
-  resetShuffle();
-}
-
-document.querySelector('.reset-all').addEventListener('click', reset);
-
-/************* RESET FUNCTION END **************/
-
-/************* MOVES FUNCTIONS START **************/
-
-let moves = 0;
-
-function addMoves() { // count player's move when they open to cards
-  moves++;
-  const movesText = document.querySelector('.moves');
-  movesText.innerHTML = moves;
-
-  if(moves == 1){
-    startClock(); // Start time when the player do the first move
-  }
-}
-
-function resetMoves(){ // reset player's moves to 0
-  moves = 0;
-  document.querySelector('.moves').innerHTML = moves;
-}
-
-/************* MOVES FUNCTIONS END **************/
-
-/************* MUHANNAD'S DEL SLUTAR **************/
-
-
 const KEY = "a2418e41e3338e1bc4c146d60794abd0";
-let searchText = "cars";
+let searchText = "Dogs";
 
 const url = `https://www.flickr.com/services/rest/?api_key=${KEY}&method=flickr.photos.search&text=${searchText}&format=json&nojsoncallback=1&per_page=12&page=1`;
 
@@ -106,12 +11,11 @@ fetch(url)
     getImageUrl(data.photos.photo);
   });
 
-//här ska vi pussla ihop bild urlen
+//pussla ihop bild urlen
 function getImageUrl(photoArray) {
   let photos = photoArray;
   let size = "q";
   let imgUrls = [];
-  
   // console.log(photos);
 
   for (let photo of photos) {
@@ -176,18 +80,21 @@ function checkForMatch() {
   if (firstCard.dataset.framework === secondCard.dataset.framework) {
     //De matchar! disable/remove event listener
     disableCards();
-    addMoves(); //////////////////// add moves function
+    addMoves(); //////////////////// Add moves function
+    scoreRating(); /////////////// Add score function
   } else {
     //De matchar inte!
     //Flippa tillbaka korten med setTimeout så vi hinner se vad som händer.
     unFlicpCards();
-    addMoves(); ////////////////// add moves function
+    addMoves(); ////////////////// Add moves function
+    losingPoints(); /////////////// Add losing
   }
 }
 
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+
   resetBoard();
 }
 
@@ -202,24 +109,126 @@ function unFlicpCards() {
 }
 
 function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false][(firstCard, secondCard)] = [ null, null ];
+  [hasFlippedCard, lockBoard] = [false, false][(firstCard, secondCard)] = [
+    null,
+    null,
+  ];
 }
 
 //Funktion som avfyras direkt och blandar korten.
+(function shuffle() {
+  cards.forEach(function (card) {
+    let randomPos = Math.floor(Math.random() * 24);
+    card.style.order = randomPos;
+  });
+})();
 
-(
-  function shuffle(){
-    cards.forEach(
-      function(card){
-        let randomPos = Math.floor(Math.random() * 24);
-        card.style.order = randomPos;
-      }
-    );
+//Sätta eventlistener på alla kort så när de klickas kallas funktionen
+//som vänder kortet
+cards.forEach((card) => card.addEventListener("click", flipCard));
+
+/************* MUHANNAD'S DEL PÅBÖRJAR **************/
+
+// Skapa en kod som frågar spelarens namn och visar den, om spelaren inte skrev namnet så visas ett namn som "Unknown"
+
+document.querySelector(".control-button button").onclick = function () {
+  const name = prompt("Whats your name?");
+
+  if (name == null || name == "") {
+    document.querySelector(".name .playerName").innerHTML = "Unknown";
+  } else {
+    document.querySelector(".name .playerName").innerHTML = name;
   }
-)();
 
+  document.querySelector(".control-button").remove();
+};
+
+function resetGame(){ // Skapa en funktion som laddar om sidan när spelaren klickar på startknappen
+  window.location.reload()
+}
+
+/************* TIMER FUNCTIONS START **************/
+
+let clockId;
+let clockOff = true;
+let time = 0;
+
+function displayTime() { // Skapa en funktion som visar tiden i sekunder och minuter
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  const clock = document.querySelector(".clock");
+  clock.innerHTML = time;
+  if (seconds < 10) {
+    clock.innerHTML = `${minutes}:0${seconds}`;
+  } else {
+    clock.innerHTML = `${minutes}:${seconds}`;
+  }
+}
+
+console.log(time)
+
+function startClock() { // Skapa en funktion som börjar räkna ut tiden
+  clockId = setInterval(function () {
+    time++;
+    displayTime();
+  }, 1000);
+}
+
+function stopClock() { // Skapa en funktion som stoppar räkna ut tiden
+  clearInterval(clockId);
+  clockOff = true;
+}
+
+function resetTimer() { // Skapa en funktion som reset tiden till 0
+  stopClock();
+  clockOff = true;
+  time = 0;
+  displayTime();
+}
+
+/************* TIMER FUNCTIONS END **************/
+
+/************* RESET FUNCTION START **************/
+
+// Skapa och lägg till en eventlyssnare till återställningsfunktionen som innehåller alla återställningsfunktioner som vi skapade
+
+function reset() {
+  resetTimer();
+  resetMoves();
+  resetShuffle();
+  resetGame();
+  resetScore()
+}
+
+document.querySelector(".reset-all").addEventListener("click", reset);
+
+/************* RESET FUNCTION END **************/
+
+/************* MOVES FUNCTIONS START **************/
+
+let moves = 0;
+
+function addMoves() { // Räkna spelarens drag när hen öppnar korten
+  moves++;
+  const movesText = document.querySelector(".moves");
+  movesText.innerHTML = moves;
+
+  if (moves == 1) {
+    startClock(); // Starttid när spelaren gör det första drag
+  }
+}
+
+function resetMoves() {
+  // återställ spelarens drag till 0
+  moves = 0;
+  document.querySelector(".moves").innerHTML = moves;
+}
+
+/************* MOVES FUNCTIONS END **************/
 
 /************* RESET SHUFFLE START ***************/
+
+// Skapa en funktion som blandar om korten när spelaren klickar på återställningsknappen
 
 function resetShuffle(){
   cards.forEach(
@@ -232,7 +241,60 @@ function resetShuffle(){
 
 /************* RESET SHUFFLE END ***************/
 
-//Sätta eventlistener på alla kort så när de klickas kallas funktionen
-//som vänder kortet
+/************* WIN FUNCTIONS START **************/
 
-cards.forEach((card) => card.addEventListener("click", flipCard));
+// Skapa en funktion som dyker upp ett gratulationsmeddelande när spelaren matchade alla kort
+
+cards.forEach(
+  function(card){
+    card.addEventListener("click",
+    function(){
+      let allmatched = document.getElementsByClassName('flip');
+      if(allmatched.length == 24){
+        setTimeout(
+          function(){
+            stopClock();
+            alert(`Congratulations! 🎉 \nYou Made (${moves}) Moves In (${time}) Sec .. Your Score Is (${score}) Point\nClikck "Ok" To Play Again!`);
+            window.location.reload()
+          }, 200
+        );
+      }
+    })
+  } 
+);
+
+/************* WIN FUNCTIONS END **************/
+
+/************* SCORE FUNCTIONS START **************/
+
+let playerScore = document.querySelector('.score');
+
+let score = 0;
+
+function scoreRating(){ // Skapa en funktion som lägger till 3 poäng om korten matchas
+
+  score += 3
+  
+  playerScore.innerHTML = score;
+  
+}
+
+function losingPoints(){ // Skapa en funktion som tar bort en poäng för varje 5 drag
+
+  if(moves == 5 || moves == 10 || moves == 15 || moves == 20 || moves == 25 || moves == 30 || moves == 35 || moves == 40 || moves == 45 || moves == 50 || moves == 55 || moves == 60 || moves == 65 || moves == 70 || moves == 75 || moves == 80 || moves == 85 || moves == 90 || moves == 95 || moves == 100){
+    score -= 1
+    playerScore.innerHTML = score;
+  }
+
+}
+
+function resetScore(){ // Skapa en funktion som reset poäng till 0
+  score = 0;
+  playerScore.innerHTML = score;
+
+}
+
+/************* SCORE FUNCTIONS END **************/
+
+
+/************* MUHANNAD'S DEL SLUTAR **************/
---
+++ JS/portotype.js
@@ -0,0 +1,51 @@
+let cardArray = [];
+let frontFaceNumb = 1;
+
+// Fixa en konstruktor för korten
+function Memorycard(_cards) {
+  this.cards = _cards;
+  this.div = document.createElement("div");
+  this.div.className = "memory-card";
+  this.frontFace = document.createElement("img");
+  this.backFace = document.createElement("img");
+  this.section = document.querySelector(".memory-game");
+  this.div.appendChild(this.frontFace);
+  this.div.appendChild(this.backFace);
+  this.section.appendChild(this.div);
+}
+
+//Prototype för attribute/class name
+Memorycard.prototype.divAttribute = function (attributeName, attributeValue) {
+  this.div.setAttribute(attributeName, attributeValue);
+};
+
+Memorycard.prototype.frontFaceClass = function (className) {
+  this.frontFace.className = className;
+};
+
+Memorycard.prototype.backFaceClass = function (className) {
+  this.backFace.className = className;
+};
+
+Memorycard.prototype.backFaceSource = function (source) {
+  this.backFace.src = source;
+};
+
+//Loopa ut kort
+for (let j = 0; j < 2; j++) {
+  // skapa 24 kort
+  for (let i = 1; i <= 12; i++) {
+    cardArray[i] = new Memorycard(i);
+
+    cardArray[i].divAttribute("data-framework", i); //lägg till attributet data-framework för att kunna matcha kort.
+    cardArray[i].frontFaceClass(
+      "front-face pair-" + frontFaceNumb
+    ); /* Lägg till class på framsidan av korten och för varje concatenate med  "frontFaceNumb" som kollar omtalet är jämt och i så fall ökar med 1 */
+    cardArray[i].backFaceClass("back-face"); //Lägg till class på backsidan av korten
+    cardArray[i].backFaceSource("../img/js-badge.svg"); //Lägg till bild för baksidan av korten.
+    if (i % 2 === 0) {
+      frontFaceNumb++;
+    }
+  }
+}
+
